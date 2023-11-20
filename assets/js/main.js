8a65e58df56e16334a5a4654a97b1be4825a06e6
NISHAN75 <nishandas880@gmail.com>
2023-11-20 16:47:16 +0600

    all is ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,8 @@
-function initOwlCarousel() {
+ 
+// nice select
+
+$('.interest-select').niceSelect();
+ function initOwlCarousel() {
     var owl = $('.building-slider');
     
     owl.owlCarousel({
@@ -38,9 +42,7 @@ initOwlCarousel();
 
 
 
-// nice select
 
-$('.interest-select').niceSelect();
 
 /* magnificPopup img view */
 $('.popup-image').magnificPopup({
@@ -78,6 +80,10 @@ $('.popup-video').magnificPopup({
 		items: 1,
 		margin: 0,
 		nav: true,
+		navText: [
+			'<svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56"><path id="Path_16625" data-name="Path 16625" d="M47.793,8.195a28,28,0,1,0,.012.012l-.012-.012m-37.13,37.13a24.5,24.5,0,1,1,.012.012Zm12-1.052a2,2,0,0,1-1.414-3.414L33.77,28.341,21.252,15.824A2,2,0,0,1,24.08,13L38.012,26.927a2,2,0,0,1,0,2.828L24.08,43.687a1.99,1.99,0,0,1-1.414.586" transform="translate(56 56) rotate(180)" fill="#fff" fill-rule="evenodd"/></svg>',
+			'<svg id="Group_731" data-name="Group 731" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="56" height="56" viewBox="0 0 56 56"><defs><clipPath id="clip-path"><rect id="Rectangle_598" data-name="Rectangle 598" width="56" height="56" fill="#fff"/></clipPath></defs><g id="Group_730" data-name="Group 730" clip-path="url(#clip-path)"><path id="Path_16625" data-name="Path 16625" d="M47.793,8.195a28,28,0,1,0,.012.012l-.012-.012m-37.13,37.13a24.5,24.5,0,1,1,.012.012Zm12-1.052a2,2,0,0,1-1.414-3.414L33.77,28.341,21.252,15.824A2,2,0,0,1,24.08,13L38.012,26.927a2,2,0,0,1,0,2.828L24.08,43.687a1.99,1.99,0,0,1-1.414.586" fill="#fff" fill-rule="evenodd"/></g></svg>'
+		  ],		  
 		dots: true,
 		autoplay: true,
         mouseDrag: false,
@@ -92,6 +98,10 @@ $('.popup-video').magnificPopup({
 		items: 1,
 		margin: 0,
 		nav: true,
+		navText: [
+			'<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="39" height="39" viewBox="0 0 39 39"><defs><clipPath id="clip-path"><rect id="Rectangle_598" data-name="Rectangle 598" width="39" height="39" transform="translate(-0.379)" fill="#97b2e6"/></clipPath></defs><g id="Group_886" data-name="Group 886" transform="translate(0 0.379)"><g id="Group_732" data-name="Group 732" transform="translate(38.621 38.621) rotate(180)"><g id="Group_730" data-name="Group 730" clip-path="url(#clip-path)"><path id="Path_16625" data-name="Path 16625" d="M32.961,5.652a19.308,19.308,0,1,0,.008.008l-.008-.008M7.354,31.259a16.9,16.9,0,1,1,.008.008Zm8.278-.726a1.379,1.379,0,0,1-.975-2.354l8.633-8.633-8.633-8.632a1.379,1.379,0,0,1,1.95-1.95l9.608,9.608a1.379,1.379,0,0,1,0,1.95l-9.608,9.608a1.373,1.373,0,0,1-.975.4" transform="translate(0 0)" fill="#97b2e6" fill-rule="evenodd"/></g></g></g></svg>',
+			'<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="38" height="39" viewBox="0 0 38 39"><defs><clipPath id="clip-path"><rect id="Rectangle_598" data-name="Rectangle 598" width="38" height="39" transform="translate(0.138 -0.379)" fill="#97b2e6"/></clipPath></defs><g id="Group_731" data-name="Group 731" transform="translate(-0.138 0.379)"><g id="Group_730" data-name="Group 730" clip-path="url(#clip-path)"><path id="Path_16625" data-name="Path 16625" d="M32.961,5.652a19.308,19.308,0,1,0,.008.008l-.008-.008M7.354,31.259a16.9,16.9,0,1,1,.008.008Zm8.278-.726a1.379,1.379,0,0,1-.975-2.354l8.633-8.633-8.633-8.632a1.379,1.379,0,1,1,1.95-1.95l9.608,9.608a1.379,1.379,0,0,1,0,1.95l-9.608,9.608a1.373,1.373,0,0,1-.975.4" transform="translate(0 0)" fill="#97b2e6" fill-rule="evenodd"/></g></g></svg>'
+		],
 		dots: true,
 		autoplay: true,
         mouseDrag: true,
@@ -151,3 +161,4 @@ openMenuBtn.addEventListener("click", () => {
 	navBar.classList.toggle("open");
 	hamburger.classList.toggle("toggle");
 });
+
